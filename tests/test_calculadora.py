ac82182d3399eb600e126450093200d72e141459
Carlos-Emmanuel-ISC6 <carlosglezalv2004@gmail,com>
2026-07-06 18:50:27 -0600

    fix: corregir errores flake8 E302 y W292

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,10 +1,13 @@
 from src.calculadora import sumar, restar, multiplicar
 
+
 def test_sumar():
     assert sumar(2, 3) == 5
 
+
 def test_restar():
     assert restar(5, 3) == 2
+
 
 def test_multiplicar():
     assert multiplicar(3, 4) == 12
